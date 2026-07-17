a7d8b6307596701f525ed887b3a0f9c799988b5c
unknown <bhumitsingh856@gmail.com>
2026-07-17 23:02:48 +0530

    leetcode 1346

--- Arrays/n_double_exist.py
+++ Arrays/n_double_exist.py
@@ -1,12 +1,10 @@
-# def double_exist(arr):
-#     for i in range(len(arr) - 1):
-#         for j in range(i + 1, len(arr)):
-#             if arr[i] * 2 == arr[j]:
-#                 return True
-#     return False
-
 def double_exist(arr):
-    arr.sort()
-    p1=0
-    p2=len(arr)-1
-print(double_exist([3, 1, 7, 11, 6]))
+    n = len(arr)
+    for i in range(n):
+        for j in range(n):
+            if i != j and arr[i] == 2 * arr[j]:
+                return True
+    return False
+
+
+print(double_exist([3, 1, 7, 11]))
