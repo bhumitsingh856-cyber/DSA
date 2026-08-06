f77b2088465f8a22efac749f4bd9f83277b3d93c
unknown <bhumitsingh856@gmail.com>
2026-08-06 23:02:20 +0530

    Leetcode 141. Linked List Cycle

---
+++ Patterns/Slow Fast Pointer/linked_list_cycle.py
@@ -0,0 +1,10 @@
+# Leetcode 141. Linked List Cycle
+
+def hasCycle(head):
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
