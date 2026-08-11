a305e64a1f8ebcdef868722c808e0230c2d949d9
unknown <bhumitsingh856@gmail.com>
2026-08-11 16:00:41 +0530

    Leetcode 287. Find the Duplicate Number

---
+++ Patterns/Slow Fast Pointer/find_duplicate_number.py
@@ -0,0 +1,20 @@
+# Leetcode 287. Find the Duplicate Number
+
+
+def duplicate(nums):
+    slow = nums[0]
+    fast = nums[0]
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+
+    fast = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    return slow
+
+
+print(duplicate([1, 3, 4, 2, 2]))
