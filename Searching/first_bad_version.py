0aa0da9dce36bd171ad8ea2997d38cce94b4e9c4
unknown <bhumitsingh856@gmail.com>
2026-08-23 20:08:01 +0530

    Leetcode 278. First Bad Version

---
+++ Searching/first_bad_version.py
@@ -0,0 +1,13 @@
+# Leetcode 278. First Bad Version
+
+def firstBadVersion(n):
+    left=1
+    right=n
+    while left<right:
+        mid=(right+left)//2
+        if(isBadVersion(mid)):
+            right=mid
+        else:
+            left=mid+1
+    return left
+    
